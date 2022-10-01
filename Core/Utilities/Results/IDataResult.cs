9dcf5548ca3dc37996377105ff3d61cd2bdcfd49
Furkan BULUT <eng.bulutt@gmail.com>
2022-10-01 11:18:09 +0300

    Add project files.

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Genel dataların(success,message) döndürüleceği interface.
+
+    /*DataResult aynı zamanda mesaj ve success
+    döndürmesi gerektiği için IResult implementasyonu yaptık*/
+    public interface IDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //this, diğer constructor u çalıştırır.
+        /*iki parametre gönderildiği zaman, Success = success
+        kodunu burada this yazarak diğer constructor'da çalıştırırız.*/
+        public Result(bool success, string message):this(success)
+        {
+          //  Success = success;
+            Message = message;  
+        }
+        //tek parametreli constructor 
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        // !!Get'ler constructor ile set edilebilir.
+    }
+}
